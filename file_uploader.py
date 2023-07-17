41a37afd13d258c2f921784e6ae7cfc2dec3ce01
Leah Chen <leahhhh1214@gmail.com>
2023-07-16 22:11:31 -0400

    Add download feature and display filename feature

--- file_uploader.py
+++ file_uploader.py
@@ -1,7 +1,11 @@
 from tkinter import Tk, Button, filedialog, Label
-from PIL import Image
+import os
+import shutil
+
+filepath = ""  # Variable to store the file path
 
 def open_file_dialog():
+    global filepath  # You need to access the global variable to save the path
     filetypes = (
         ("PDF Files", "*.pdf"),
         ("Image Files", "*.jpg;*.jpeg;*.png;*.gif")
@@ -10,11 +14,14 @@ def open_file_dialog():
     if filepath:
         # Perform actions with the selected file
         print("Selected File:", filepath)
-        
-        # For image files, you can also use PIL to open and display the image
-        if filepath.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            image = Image.open(filepath)
-            image.show()
+        file_label.config(text="Uploaded File: " + os.path.basename(filepath))
+
+def download_file():
+    global filepath  # Access the global variable
+    if filepath:
+        download_path = filedialog.asksaveasfilename(defaultextension='*.*')
+        if download_path:
+            shutil.copy2(filepath, download_path)
 
 # Create the Tkinter window
 window = Tk()
@@ -26,8 +33,16 @@ label = Label(window, text="Upload file to Textract")
 label.pack(pady=10)
 
 # Create the button to open the file dialog
-button = Button(window, text="Upload File", command=open_file_dialog)
-button.pack()
+upload_button = Button(window, text="Upload File", command=open_file_dialog)
+upload_button.pack()
+
+# Create a label to display the file name
+file_label = Label(window, text="Uploaded File:")
+file_label.pack(pady=10)
+
+# Create the button to download the uploaded file
+download_button = Button(window, text="Download File", command=download_file)
+download_button.pack()
 
 # Run the Tkinter event loop
 window.mainloop()
