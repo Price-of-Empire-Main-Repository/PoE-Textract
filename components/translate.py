bdd0002fb154fce2b3f49d50e1cc4db7bf16976a
Leah Chen <leahhhh1214@gmail.com>
2023-08-11 12:33:58 -0400

    Create translate.py

---
+++ components/translate.py
@@ -0,0 +1,13 @@
+import boto3
+
+def translate(response, srcLang, targLang):
+    # Amazon Translate client
+    translate = boto3.client('translate')
+
+    print ('')
+    for item in response["Blocks"]:
+        if item["BlockType"] == "LINE":
+            print ('\033[94m' +  item["Text"] + '\033[0m')
+            result = translate.translate_text(Text=item["Text"], SourceLanguageCode="en", TargetLanguageCode="de")
+            print ('\033[92m' + result.get('TranslatedText') + '\033[0m')
+        print ('')
